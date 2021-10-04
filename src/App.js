ede099e6bbf603f90a62dbaecd1ee972789d137d
MinhajSadik <MinhajoSadik@gmail.com>
2021-10-04 23:46:40 +0600

    [remove] one file which is not used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Contacts from './Module/Contacts/Contacts';
-import TrainingAddOption from './Module/Contacts/TrainingAddOption';
+// import TrainingAddOption from './Module/Contacts/TrainingAddOption';
 
 const App = () => {
     return (
         <div className="App">
             <Contacts/>
-            <TrainingAddOption/>
+            {/* <TrainingAddOption/> */}
         </div>
     
     );
